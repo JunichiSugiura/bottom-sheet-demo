a3ce0ee215d9df07254b1fda07cc0c11d731c287
Luke Sugiura <jun.sugiura.jp@gmail.com>
2018-09-15 21:28:19 +0900

    [WIP] bug: initial opacity is set to 1

--- src/app.js
+++ src/app.js
@@ -1,33 +1,63 @@
 import React from 'react'
 import {
+  Animated,
   Dimensions,
+  Text,
+  TouchableWithoutFeedback,
   StyleSheet,
   View
 } from 'react-native'
 import Interactable from 'react-native-interactable'
 
-export default () => (
+export default ({ position }) => (
   <View style={styles.container}>
+    <Text>Content goes here</Text>
+    <TouchableWithoutFeedback style={styles.shadowTouchable}>
+      <Animated.View style={[styles.shadow, {
+        opacity: position.interpolate({
+          inputRange: [0, INITIAL_MARGIN_TOP],
+          outputRange: [1, 0]
+        })
+      }]} />
+    </TouchableWithoutFeedback>
     <Interactable.View
       verticalOnly
-      initialPosition={{ y: HEIGHT - INITIAL_HEIGHT }}
-      snapPoints={[{y: 0}, { y: HEIGHT - INITIAL_HEIGHT }, { y: HEIGHT - BOX_WIDTH }]}>
-      <View style={styles.box} /> 
+      initialPosition={{ y: INITIAL_MARGIN_TOP }}
+      snapPoints={[{ y: 0 }, { y: INITIAL_MARGIN_TOP }, { y: HEIGHT - WIDTH }]}
+      animatedValueY={position}
+      style={{position: 'absolute'}}>
+      <View style={styles.bottomSheet} /> 
     </Interactable.View>
   </View>
 )
 
 const HEIGHT = Dimensions.get('window').height 
-const BOX_WIDTH = Dimensions.get('window').width
+const WIDTH = Dimensions.get('window').width
 const INITIAL_HEIGHT = 80
+const INITIAL_MARGIN_TOP = HEIGHT - INITIAL_HEIGHT
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    alignItems: 'center',
+    flex: 1,
+    paddingTop: 40
   },
-  box: {
-    backgroundColor: 'cyan',
+  shadow: {
+    backgroundColor: 'black',
     height: HEIGHT,
-    width: BOX_WIDTH
-  }
+    position: 'absolute',
+    width: WIDTH
+  },
+  shadowTouchable: {
+    backgroundColor: 'transparent',
+    height: HEIGHT,
+    position: 'absolute',
+    width: WIDTH
+  },
+  bottomSheet: {
+    backgroundColor: 'cyan',
+    borderRadius: 8,
+    height: HEIGHT,
+    width: WIDTH,
+  },
 })
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import { Animated } from 'react-native'
+import {
+  compose,
+  withProps
+} from 'recompose'
 import App from './app'
 
-export default App
+export default compose(
+  withProps({
+    position: new Animated.Value(0)
+  })
+)(App)
